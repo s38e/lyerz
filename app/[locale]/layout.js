7b583a96ae6b0c2aafe37cbc28a8d895fc3061b7
Saeed Khaled <saeedpop1999@gmail.com>
2024-03-17 18:05:49 +0200

    First Commit

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,33 +1,5 @@
-// import { useLocale } from "next-intl";
-// import { notFound } from "next/navigation";
-// import { Inter } from "next/font/google";
-// import "../globals.css";
-
-// const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata = {
-//   title: "Lyerz",
-//   description: "description Lyerz",
-// };
-
-// export default function RootLayout({ children, params }) {
-//   //  GET THE CURRENT LOCALE
-//   const locale = useLocale();
-//   // Show 404 Error if language not found
-//   if (params.locale !== locale) {
-//     notFound();
-//   }
-//   return (
-//     <html lang={locale}>
-//       <body className={inter.className}>{children}</body>
-//     </html>
-//   );
-// }
-
-// استيراد الدوال المطلوبة من `next/navigation` بدلاً من `next/router`
-import { notFound } from "next/navigation";
-// استخدام هذه الدوال بدلاً من `useRouter()` في Server Components
 import { useLocale } from "next-intl";
+import { notFound } from "next/navigation";
 import { Inter } from "next/font/google";
 import "../globals.css";
 
@@ -38,18 +10,46 @@ export const metadata = {
   description: "description Lyerz",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, params }) {
+  //  GET THE CURRENT LOCALE
   const locale = useLocale();
-
-  // استخدام `notFound()` بدلاً من `router.locale !== locale`
-  if (locale !== "en" && locale !== "ar") {
+  // Show 404 Error if language not found
+  if (params.locale !== locale) {
     notFound();
-    return null;
   }
-
   return (
     <html lang={locale}>
       <body className={inter.className}>{children}</body>
     </html>
   );
 }
+
+// // استيراد الدوال المطلوبة من `next/navigation` بدلاً من `next/router`
+// import { notFound } from "next/navigation";
+// // استخدام هذه الدوال بدلاً من `useRouter()` في Server Components
+// import { useLocale } from "next-intl";
+// import { Inter } from "next/font/google";
+// import "../globals.css";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata = {
+//   title: "Lyerz",
+//   description: "description Lyerz",
+// };
+
+// export default function RootLayout({ children }) {
+//   const locale = useLocale();
+
+//   // استخدام `notFound()` بدلاً من `router.locale !== locale`
+//   if (locale !== "ar" && locale !== "en") {
+//     notFound();
+//     return null;
+//   }
+
+//   return (
+//     <html lang={locale}>
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   );
+// }
