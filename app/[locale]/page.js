52a3b26831656bba666871f6fac907dc7725c89c
Saeed Khaled <saeedpop1999@gmail.com>
2024-03-18 22:43:25 +0200

    Last Update at 2024/03/18 10:43PM

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -50,7 +50,21 @@ export default function Home() {
     gsap.ticker.add((time) => {
       lenis.raf(time * 1000);
     });
+    // ----------- Transform Slider ----------- //
+    gsap.to(`.${styles.recentWork}`, {
+      x: 300,
+      duration: 1,
+      scrollTrigger: {
+        trigger: `.${styles.sliders}`,
+        scrub: 1,
+        start: "top 60%",
+        end: "bottom 0%",
+        toggleActions: "play none none none",
+        // markers: true,
+      },
+    });
   }, []);
+
   // const t = useTranslations("Heading");
   return (
     <>
@@ -88,8 +102,30 @@ export default function Home() {
                 <Image src={img08} alt="img08" />
                 <Image src={img09} alt="img09" />
               </div>
+              <div className={styles.recentWork_slide}>
+                <Image src={img09} alt="img09" />
+                <Image src={img08} alt="img08" />
+                <Image src={img07} alt="img07" />
+                <Image src={img06} alt="img06" />
+                <Image src={img05} alt="img05" />
+                <Image src={img04} alt="img04" />
+                <Image src={img03} alt="img03" />
+                <Image src={img02} alt="img02" />
+                <Image src={img01} alt="img01" />
+              </div>
             </div>
             <div className={styles.recentWork}>
+              <div className={styles.recentWork_slide}>
+                <Image src={img09} alt="img09" />
+                <Image src={img08} alt="img08" />
+                <Image src={img07} alt="img07" />
+                <Image src={img06} alt="img06" />
+                <Image src={img05} alt="img05" />
+                <Image src={img04} alt="img04" />
+                <Image src={img03} alt="img03" />
+                <Image src={img02} alt="img02" />
+                <Image src={img01} alt="img01" />
+              </div>
               <div className={styles.recentWork_slide}>
                 <Image src={img09} alt="img09" />
                 <Image src={img08} alt="img08" />
@@ -233,6 +269,109 @@ export default function Home() {
                   <p>Billboards</p>
                   <span className={styles.dot}>•</span>
                 </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className={`${styles.section_3} ${styles.section}`}>
+          <div className={styles.left}>
+            {/* <Image src={Logo} alt="logo" className={styles.overlayImage} /> */}
+            <div className={styles.top}>
+              <div className={styles.available_bell}>
+                <div className={styles.circle}></div>
+                <p>Slots available</p>
+              </div>
+              <h1 className={styles.headText}>Join Lyerz</h1>
+            </div>
+            <div className={styles.bottom}>
+              <div className={styles.btn}>
+                <Link href="/">
+                  <div className={styles.texts}>
+                    <h5>Book a 15-min intro call</h5>
+                    <p>
+                      Learn more about how DesignJoy works and how it can help
+                      you.
+                    </p>
+                  </div>
+                  <svg
+                    width="21"
+                    height="17"
+                    viewBox="0 0 21 17"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4.08049 8.16093L20.9124 8.16093C17.342 8.16093 12.2414 6.1207 12.2414 0"
+                      stroke="#FF90E7"
+                      stroke-width="0.995828"
+                    />
+                    <path
+                      d="M0.000167847 8.16084L20.9126 8.16084C17.3422 8.16084 12.2416 10.2011 12.2416 16.3218"
+                      stroke="#FF90E7"
+                      stroke-width="0.995828"
+                    />
+                  </svg>
+                </Link>
+              </div>
+              <div className={styles.btn}>
+                <Link href="/">
+                  <div className={styles.texts}>
+                    <h5>Refer a friend & earn</h5>
+                    <p>
+                      Earn 5% monthly recurring commissions for each referral.
+                    </p>
+                  </div>
+                  <svg
+                    width="21"
+                    height="17"
+                    viewBox="0 0 21 17"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4.08049 8.16093L20.9124 8.16093C17.342 8.16093 12.2414 6.1207 12.2414 0"
+                      stroke="#FF90E7"
+                      stroke-width="0.995828"
+                    />
+                    <path
+                      d="M0.000167847 8.16084L20.9126 8.16084C17.3422 8.16084 12.2416 10.2011 12.2416 16.3218"
+                      stroke="#FF90E7"
+                      stroke-width="0.995828"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.plan}>
+              <h4>Membership</h4>
+              <div className={styles.switchs}>
+                <div className={styles.switch}>Go</div>
+                <div className={styles.switch}>Startup</div>
+                <div className={styles.switch}>Pro</div>
+              </div>
+              <div className={styles.saliry}>
+                <h5>
+                  $XXXX<span>/m</span>
+                </h5>
+                <p>
+                  <span>one request at a time.</span> pause or cancel anytime.
+                </p>
+              </div>
+              <div className={styles.line}></div>
+              <div className={styles.bottom}>
+                <h5>What's included /</h5>
+                <div className={styles.advantages}>
+                  <p>▪ one request at a time</p>
+                  <p>▪ average 3 days delivery</p>
+                  <p>▪ unlimited brands</p>
+                  <p>▪ unlimited users</p>
+                  <p>▪ dashboard or slack</p>
+                  <p>▪ easy credit-card payments</p>
+                  <p>▪ pause or cancel anytime</p>
+                </div>
+                <button className={styles.btn}>Get started</button>
               </div>
             </div>
           </div>
