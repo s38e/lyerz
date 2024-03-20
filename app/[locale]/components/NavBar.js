5aa39272cbafe974c6fec20f142794ed87b3aad9
Saeed Khaled <saeedpop1999@gmail.com>
2024-03-20 16:26:56 +0200

    Last Update at 2024/03/20 04:26PM

--- app/[locale]/components/NavBar.js
+++ app/[locale]/components/NavBar.js
@@ -2,8 +2,14 @@ import styles from "./styles/navbar.module.css";
 import Logo from "@/public/assets/logo.png";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 function NavBar() {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const handleHamburgerClick = () => {
+    setActiveIndex(activeIndex === null ? "active" : null);
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
@@ -23,6 +29,16 @@ function NavBar() {
           Book a demo
         </Link>
       </div>
+      <button
+        onClick={handleHamburgerClick}
+        className={`${styles.hamburger} ${activeIndex ? styles.active : ""}`}
+      >
+        <span></span>
+        <span></span>
+      </button>
+      <div
+        className={`${styles.menu} ${activeIndex ? styles.active : ""}`}
+      ></div>
     </nav>
   );
 }
--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -421,8 +421,34 @@ export default function Home() {
                 <Image src={img02} alt="img02" />
                 <Image src={img01} alt="img01" />
               </div>
+              <div className={styles.recentWork_slide}>
+                <Image src={img09} alt="img09" />
+                <Image src={img02} alt="img02" />
+                <Image src={img08} alt="img08" />
+                <Image src={img07} alt="img07" />
+                <Image src={img06} alt="img06" />
+                <Image src={img09} alt="img09" />
+                <Image src={img05} alt="img05" />
+                <Image src={img04} alt="img04" />
+                <Image src={img03} alt="img03" />
+                <Image src={img02} alt="img02" />
+                <Image src={img01} alt="img01" />
+              </div>
             </div>
             <div className={styles.recentWork}>
+              <div className={styles.recentWork_slide}>
+                <Image src={img09} alt="img09" />
+                <Image src={img02} alt="img02" />
+                <Image src={img08} alt="img08" />
+                <Image src={img07} alt="img07" />
+                <Image src={img06} alt="img06" />
+                <Image src={img09} alt="img09" />
+                <Image src={img05} alt="img05" />
+                <Image src={img04} alt="img04" />
+                <Image src={img03} alt="img03" />
+                <Image src={img02} alt="img02" />
+                <Image src={img01} alt="img01" />
+              </div>
               <div className={styles.recentWork_slide}>
                 <Image src={img09} alt="img09" />
                 <Image src={img02} alt="img02" />
