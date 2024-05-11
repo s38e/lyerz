ce22fc39dd73b1801737548e0f67d332150962c7
Saeed Khaled <saeedpop1999@gmail.com>
2024-05-11 18:32:24 +0300

    Last Update at '2024/05/11' 06:31AM | | Test Multi language

---
+++ src/app/[locale]/layout.js
@@ -0,0 +1,34 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import "../globals.css";
+
+/**
+ * @typedef {Object} LocaleLayoutProps
+ * @property {React.ReactNode} children
+ * @property {Object} params
+ * @property {string} params.locale
+ */
+
+/**
+ * @param {LocaleLayoutProps} props
+ */
+export default async function LocaleLayout(props) {
+  const {
+    children,
+    params: { locale },
+  } = props;
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/[locale]/page.js
@@ -0,0 +1,15 @@
+"use client";
+import Image from "next/image";
+import styles from "../page.module.css";
+import { useTranslations } from "next-intl";
+import { useEffect } from "react";
+
+// export default function Home() {
+//   return <main className={styles.main}></main>;
+// }
+
+export default function Home() {
+  useEffect(() => {});
+  const t = useTranslations("Index");
+  return <main className={styles.page}></main>;
+}
