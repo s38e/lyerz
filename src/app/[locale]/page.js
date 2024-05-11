36a8d91342873bb8cbbbdf7a29731b818f16010e
Saeed Khaled <saeedpop1999@gmail.com>
2024-05-11 18:46:56 +0300

    Last Update at '2024/05/11' 06:35AM | | Test NextSeo

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 import styles from "../page.module.css";
 import { useTranslations } from "next-intl";
 import { useEffect } from "react";
+import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 // export default function Home() {
 //   return <main className={styles.main}></main>;
@@ -11,5 +13,13 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {});
   const t = useTranslations("Index");
-  return <main className={styles.page}>تيستتتتتتتتتتتتتتتتت</main>;
+  return (
+    <>
+      <NextSeo
+        title="Lyerz - Branding Agency"
+        description="تاب يستخدم لعرض الصفحة الرئيسية للموقع، حيث يتم استيراد العديد من المكتبات مثل 'use client' و 'next/image' و 'next-intl'، وتعريف العناصر الرئيسية للصفحة مثل 'main' و 'styles' و 'useTranslations'. تم تعليق الدالة الرئيسية وإعادة تعريفها باستخدام 'useEffect' و 'useTranslations' لإعداد بعض العمليات اللازمة لعرض المحتوى بشكل صحيح."
+      />
+      <main className={styles.page}></main>;
+    </>
+  );
 }
