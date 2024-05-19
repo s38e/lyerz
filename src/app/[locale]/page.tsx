ad7c815e35da2fc8db5094eadf0400c0c1c1a0b1
Saeed Khaled <saeedpop1999@gmail.com>
2024-05-19 22:45:34 +0300

    ss

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,21 +1,7 @@
 import React from "react";
 import HomePage from "../pages/HomePage";
-import { Metadata } from "next";
 import styles from "../page.module.css";
 
-export const metadata: Metadata = {
-  title: "Home",
-  openGraph: {
-    title: "Lyerz - Desgin Studio",
-    description: "Dev with Lyerz by Saeed Khaled",
-    images: {
-      url: "/assets/Opengraph-Image.png",
-      width: 1200,
-      height: 630,
-      alt: "Lyerz - Design Studio",
-    },
-  },
-};
 export default function Home() {
   return (
     <>
