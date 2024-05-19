102afa4542774a202bc9417fb2fb5e6428064275
Saeed Khaled <saeedpop1999@gmail.com>
2024-05-19 12:44:47 +0300

    Last Update at '2024/05/19' 12:33AM || Setting the SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Metadata } from "next";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
@@ -13,11 +10,13 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Lyerz - Desgin Studio",
     description: "Dev with Lyerz by Saeed Khaled",
-    images: {
-      url: "../../assets/Opengraph-Image.png",
-      width: 1200,
-      height: 630,
-    },
+    images: [
+      {
+        url: "../../assets/Opengraph-Image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
@@ -29,7 +28,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
