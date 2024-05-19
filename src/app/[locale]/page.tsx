4fe158ad5344ee407a8dd0e2e66fb5a683aebb7b
Saeed Khaled <saeedpop1999@gmail.com>
2024-05-19 12:47:39 +0300

    Last Update at '2024/05/19' 12:47AM || Setting the SEO

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,6 +4,17 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Home",
+  openGraph: {
+    title: "Lyerz - Desgin Studio",
+    description: "Dev with Lyerz by Saeed Khaled",
+    images: [
+      {
+        url: "../../../assets/Opengraph-Image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 export default function Home() {
   return (
