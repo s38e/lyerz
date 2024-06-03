f21e99578256b0fd28f47262d2d194d7a3c0ef7f
Saeed Khaled <saeedpop1999@gmail.com>
2024-06-03 11:56:41 +0300

    Last Update at '2024/06/03' 11:56PM || edits in Overlay Plans

--- src/app/pages/HomePage.js
+++ src/app/pages/HomePage.js
@@ -71,7 +71,7 @@ function HomePage() {
   const [isCardWorkClicked, setIsCardWorkClicked] = useState(false);
   const [isCardServicesClicked, setIsCardServicesClicked] = useState(false);
   const [isCardAboutClicked, setIsCardAboutClicked] = useState(false);
-  const [isCardPlansClicked, setIsCardPlansClicked] = useState(true);
+  const [isCardPlansClicked, setIsCardPlansClicked] = useState(false);
 
   useEffect(() => {
     const Footer = document.querySelector(`.${FooterStyles.Footer}`);
