99247fe29367ec01c01a71e079aa9f39036c0e88
Saeed Khaled <saeedpop1999@gmail.com>
2024-06-05 22:32:02 +0300

    Last Update at '2024/06/05' 10:31AM || Responsive Styles

--- src/app/pages/HomePage.js
+++ src/app/pages/HomePage.js
@@ -65,6 +65,7 @@ import Link from "next/link";
 import { isMobile } from "react-device-detect";
 
 function HomePage() {
+  const [isCloseAnyOverlayActive, setIsCloseAnyOverlayActive] = useState(false);
   // ---------------- Hovered ---------------- //
   const [isCardWorkHovered, setIsCardWorkHovered] = useState(false);
   const [isCardServicesHovered, setIsCardServicesHovered] = useState(false);
@@ -101,6 +102,16 @@ function HomePage() {
       } else {
         Footer.style.gap = "1rem";
       }
+    }
+    if (
+      isCardWorkClicked ||
+      isCardServicesClicked ||
+      isCardAboutClicked ||
+      isCardPlansClicked
+    ) {
+      setIsCloseAnyOverlayActive(true);
+    } else {
+      setIsCloseAnyOverlayActive(false);
     }
     // ---------------- hover effect on cards ---------------- //
     const cards = document.querySelectorAll(
@@ -240,6 +251,21 @@ function HomePage() {
       { opacity: 1, delay: 0.9, y: 0, duration: 1.25, ease: "power4.out" }
     );
   }, []);
+
+  // ---------------- Click Work Card ---------------- //
+  const CloseAnyOverlay = () => {
+    if (
+      isCardWorkClicked ||
+      isCardServicesClicked ||
+      isCardAboutClicked ||
+      isCardPlansClicked
+    ) {
+      setIsCardWorkClicked(false);
+      setIsCardServicesClicked(false);
+      setIsCardAboutClicked(false);
+      setIsCardPlansClicked(false);
+    }
+  };
 
   // ---------------- Click Work Card ---------------- //
   const handleCardWorkClick = () => {
@@ -1060,6 +1086,26 @@ function HomePage() {
             </div>
             <div></div>
           </div>
+        </div>
+        <div
+          className={`${styles.btnCloseAnyOverlay} ${
+            isCloseAnyOverlayActive ? styles.active : ""
+          }`}
+          onClick={CloseAnyOverlay}
+        >
+          Close
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M25.3333 15.667V16.3336C25.3333 16.7018 25.0349 17.0003 24.6667 17.0003H17V24.667C17 25.0351 16.7015 25.3336 16.3333 25.3336H15.6667C15.2985 25.3336 15 25.0351 15 24.667V17.0003H7.3333C6.96511 17.0003 6.66663 16.7018 6.66663 16.3336V15.667C6.66663 15.2988 6.96511 15.0003 7.3333 15.0003H15V7.33365C15 6.96546 15.2985 6.66699 15.6667 6.66699H16.3333C16.7015 6.66699 17 6.96546 17 7.33365V15.0003H24.6667C25.0349 15.0003 25.3333 15.2988 25.3333 15.667Z"
+              fill="currentColor"
+            ></path>
+          </svg>
         </div>
       </section>
     </main>
