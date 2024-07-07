0f7509709b2a2e6c64980438bc4a70dcd90f5d8e
Saeed Khaled <saeedpop1999@gmail.com>
2024-07-07 21:17:19 +0300

    Last Update at '2024/07/07' 09:17PM || Final Edit

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://example.com",
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://lyerz.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   outDir: "./public",
