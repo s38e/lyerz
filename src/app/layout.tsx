21a20c2a0e793e45cb16d28995657668632433a2
Saeed Khaled <saeedpop1999@gmail.com>
2024-07-07 23:00:12 +0300

    Last Update at '2024/07/07' 11:00PM || Final Edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,18 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://lyerz.com/"),
-  title: {
-    default: "LYERZ | Design Subscription for Scale-Ups",
-    template: "%s | LYERZ | Design Subscription for Scale-Ups",
-  },
+  title: "LYERZ | Design Subscription for Scale-Ups",
   description: "Hire Your Whole Design Team With a Few Clicks.",
   openGraph: {
+    url: "https://lyerz.com/",
+    siteName: "LYERZ | Design Subscription for Scale-Ups",
     title: "LYERZ | Design Subscription for Scale-Ups",
     description: "Hire Your Whole Design Team With a Few Clicks.",
     images: {
       url: "/assets/og.png",
       width: 1200,
       height: 630,
+      alt: "LYERZ | Design Subscription for Scale-Ups",
     },
   },
   twitter: {
