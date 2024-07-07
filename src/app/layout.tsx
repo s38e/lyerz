60424a18ca4f1589d38e6bba633b91a63b5af91a
Saeed Khaled <saeedpop1999@gmail.com>
2024-07-07 22:45:42 +0300

    Last Update at '2024/07/07' 10:45PM || Final Edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
     title: "LYERZ | Design Subscription for Scale-Ups",
     description: "Hire Your Whole Design Team With a Few Clicks.",
     images: {
-      url: "/assets/og.png",
+      url: "/assets/Opengraph-Image.jpg",
       width: 1200,
       height: 630,
     },
