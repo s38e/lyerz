4b589cd109255a36a8e4510123e29b4f1b2cf1e4
Saeed Khaled <saeedpop1999@gmail.com>
2024-07-08 16:29:15 +0300

    Last Update at '2024/07/08' 04:29PM || Final Edit

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,10 +3,11 @@ import HomePage from "../pages/HomePage";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://lyerz.vercel.app/"),
+  metadataBase: new URL("https://lyerz.com/"),
   title: "LYERZ | Design Subscription for Scale-Ups",
   description: "Hire Your Whole Design Team With a Few Clicks.",
   openGraph: {
+    url: "https://lyerz.com/",
     siteName: "LYERZ | Design Subscription for Scale-Ups",
     title: "LYERZ | Design Subscription for Scale-Ups",
     description: "Hire Your Whole Design Team With a Few Clicks.",
@@ -16,11 +17,6 @@ export const metadata: Metadata = {
       height: 630,
       alt: "LYERZ | Design Subscription for Scale-Ups",
     },
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "LYERZ | Design Subscription for Scale-Ups",
-    description: "Hire Your Whole Design Team With a Few Clicks.",
   },
 };
 
